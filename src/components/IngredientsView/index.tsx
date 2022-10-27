01b40793db4d6e9602b4c45b1a06ebb6c7535a99
Verathagnus <59093644+Verathagnus@users.noreply.github.com>
2022-10-28 02:58:27 +0530

    basic ingredient view and recipe view design

--- src/components/IngredientsView/index.tsx
+++ src/components/IngredientsView/index.tsx
@@ -1,33 +1,79 @@
-import React, { useEffect } from "react";
-import { getRecipesListAlphabetical } from "../../services/recipeService";
+import React, { Fragment, useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../store";
+import { IngredientGetBasicType } from "../../types/types";
 import {
   fetchIngredientsPopular,
   selectIngredients,
 } from "../../store/ingredient/ingredientSlice";
+
 const IngredientsView = () => {
   const dispatch = useAppDispatch();
   const ingredientsList = useAppSelector(selectIngredients);
   useEffect(() => {
-    dispatch(fetchIngredientsPopular()).then(() => console.log(ingredientsList));
+    dispatch(fetchIngredientsPopular()).then(() =>
+      console.log(ingredientsList)
+    );
   });
   return (
     <>
-      <div className="px-10 w-[90%]  text-center mx-auto drop-shadow-lg pb-20">
-        <h3 className="font-medium text-left leading-tight text-3xl mt-0 mb-2 text-red-600 pb-5 pt-10">
-        IngredientsView
+      <div className="px-10 w-[90%]  mx-auto drop-shadow-lg pb-20">
+        <h3 className="font-medium text-left leading-tight text-3xl mt-0 mb-2 text-red-600 pt-10">
+          Ingredients
         </h3>
-      {ingredientsList && ingredientsList.map((ingredient) => {
-        return (
-          <>
-          <p className="break-words">{JSON.stringify(ingredient)}</p>
-          </>
-        )
-      })}
+        <div className="grid lg:grid-cols-2 lg:gap-4">
+          {ingredientsList &&
+            ingredientsList.map((ingredient) => {
+              return (
+                <Fragment key={ingredient._id}>
+                  {/* <p className="break-words">{JSON.stringify(ingredient)}</p> */}
+                  <div className="max-w-full sm:max-w-full md:max-w-full w-full lg:max-w-full lg:flex pt-5">
+                    <div
+                      className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+                      // style="background-image: url('/img/card-left.jpg')"
+                      style={{
+                        backgroundImage: `url(${ingredient.uploadedIngredientImage})`,
+                      }}
+                      title={ingredient.name}
+                    >
+                      {/* <img src={ingredient.uploadedRecipeImageFileName}></img> */}
+                    </div>
+                    <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex md:flex-col justify-between leading-normal lg:w-[300px] flex-row">
+                      <div className="mb-2">
+                        <div className="text-sm text-gray-600 flex items-center">
+                          {ingredient.category === "Veg" ? (
+                            <p className="text-green-500">Veg</p>
+                          ) : (
+                            <p className="text-red-600">Non-Veg</p>
+                          )}
+                        </div>
+                        <div className="text-gray-900 font-bold text-xl">
+                          {ingredient.name}
+                        </div>
+                        <p className="text-gray-700 text-base">
+                          {ingredient.ingredientText}
+                        </p>
+                      </div>
+                      <div>
+                        <span className="text-blue-500">Filtered + </span>
+                        <span>{ingredient.filter_plus}</span>
+                      </div>
+                      <div>
+                        <span className="text-red-500">Filtered - </span>
+                        <span>{ingredient.filter_minus}</span>
+                      </div>
+                      <div>
+                        <span className="text-yellow-500">PI </span>
+                        <span>{ingredient.popularity}</span>
+                      </div>
+                    </div>
+                  </div>
+                </Fragment>
+              );
+            })}
+        </div>
       </div>
     </>
   );
 };
 
 export default IngredientsView;
-
--- src/components/RecipesView/index.tsx
+++ src/components/RecipesView/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import { getRecipesListAlphabetical } from "../../services/recipeService";
 import { useAppDispatch, useAppSelector } from "../../store";
 import {
@@ -18,62 +18,75 @@ const RecipesView = () => {
         <h3 className="font-medium text-left leading-tight text-3xl mt-0 mb-2 text-red-600 pt-10">
           Recipes
         </h3>
-        {recipesList &&
-          recipesList.map((recipe) => {
-            return (
-              <>
-                {/* <p className="break-words">{JSON.stringify(recipe)}</p> */}
-                <div className="max-w-sm w-full lg:max-w-full lg:flex pt-5">
-                  <div
-                    className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-                    // style="background-image: url('/img/card-left.jpg')"
-                    style={{"backgroundImage": (`url(${recipe.uploadedRecipeImageFileName})`)}}
-                    title={recipe.name}
-                  >
-                    {/* <img src={recipe.uploadedRecipeImageFileName}></img> */}
-                  </div>
-                  <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-                    <div className="mb-8">
-                      <p className="text-sm text-gray-600 flex items-center">
-                        {recipe.ingredientsRequired.reduce(
-                          (categoryPrev: string, ingredient) => {
-                            if (ingredient.category === "Non-Veg") {
-                              return "Non-Veg";
-                            }
-                            return categoryPrev;
-                          },
-                          "Veg"
-                        ) === "Veg" ? <p className="text-green-500">Veg</p> : <p className="text-red-600">Non-Veg</p> }
-                      </p>
-                      <div className="text-gray-900 font-bold text-xl">
-                        {recipe.name}
-                      </div>
-                      <p className="text-gray-700 text-base">
-                        {recipe.recipeText}
-                      </p>
+        <div className="grid lg:grid-cols-2 lg:gap-4">
+          {recipesList &&
+            recipesList.map((recipe) => {
+              return (
+                <Fragment key={recipe._id}>
+                  {/* <p className="break-words">{JSON.stringify(recipe)}</p> */}
+                  <div className="max-w-full sm:max-w-full md:max-w-full w-full lg:max-w-full lg:flex pt-5">
+                    <div
+                      className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+                      // style="background-image: url('/img/card-left.jpg')"
+                      style={{
+                        backgroundImage: `url(${recipe.uploadedRecipeImageFileName})`,
+                      }}
+                      title={recipe.name}
+                    >
+                      {/* <img src={recipe.uploadedRecipeImageFileName}></img> */}
                     </div>
-                    {recipe.ingredientsRequired.map((ingredient) => {
-                      return (
-                        <div className="flex items-center pb-2">
-                          <img
-                            className="w-10 h-10 rounded-full mr-4"
-                            src={ingredient.uploadedIngredientImage}
-                            alt={ingredient.name}
-                          />
-                          <div className="text-sm">
-                            <p className="text-gray-900 leading-none">
-                              {ingredient.name}
-                            </p>
-                            <p className="text-gray-600">{ingredient.category}</p>
-                          </div>
+                    <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal lg:w-[300px]">
+                      <div className="mb-8">
+                        <div className="text-sm text-gray-600 flex items-center">
+                          {recipe.ingredientsRequired.reduce(
+                            (categoryPrev: string, ingredient) => {
+                              if (ingredient.category === "Non-Veg") {
+                                return "Non-Veg";
+                              }
+                              return categoryPrev;
+                            },
+                            "Veg"
+                          ) === "Veg" ? (
+                            <p className="text-green-500">Veg</p>
+                          ) : (
+                            <p className="text-red-600">Non-Veg</p>
+                          )}
                         </div>
-                      );
-                    })}
+                        <div className="text-gray-900 font-bold text-xl">
+                          {recipe.name}
+                        </div>
+                        <p className="text-gray-700 text-base">
+                          {recipe.recipeText}
+                        </p>
+                      </div>
+                      {recipe.ingredientsRequired.map((ingredient) => {
+                        return (
+                          <div
+                            className="flex items-center pb-2"
+                            key={ingredient._id}
+                          >
+                            <img
+                              className="w-10 h-10 rounded-full mr-4"
+                              src={ingredient.uploadedIngredientImage}
+                              alt={ingredient.name}
+                            />
+                            <div className="text-sm">
+                              <p className="text-gray-900 leading-none">
+                                {ingredient.name}
+                              </p>
+                              <p className="text-gray-600">
+                                {ingredient.category}
+                              </p>
+                            </div>
+                          </div>
+                        );
+                      })}
+                    </div>
                   </div>
-                </div>
-              </>
-            );
-          })}
+                </Fragment>
+              );
+            })}
+        </div>
       </div>
     </>
   );
